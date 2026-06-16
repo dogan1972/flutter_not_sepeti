51adecd0369a9998c7e6f0b0cb3bd3d8523b4f09
Doğan KANLIPIÇAK <dogan1972@gmail.com>
2026-06-16 10:19:01 +0300

    dördüncü commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_not_sepeti/kategori_ekle.dart';
+import 'package:flutter_not_sepeti/kategori_ekle_page.dart';
 import 'package:flutter_not_sepeti/utils/database_helper.dart';
 
 void main() {
